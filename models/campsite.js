a57639d53938c57ebb75f734a9cb3c57a2de5566
Walter Neto <wneto.macedo@gmail.com>
2020-11-27 16:33:01 -0600

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/campsite.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    test: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true //Makes it so that no two documents can have the same name
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema] //Allows campsite to have comments held in an array
+}, {
+    timestamps: true
+});
+
+const Campsite = mongoose.model('Campsite', campsiteSchema) //Mongoose will look for the plural version of the collection title "campsites"
+
+module.exports = Campsite;
